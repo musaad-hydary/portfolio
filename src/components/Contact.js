a79d56670ce19f54ba17a1e52246a9319a45aa4c
musaad <musaadhydary@gmail.com>
2024-02-23 20:44:12 -0500

    optimized loading and images

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -22,27 +22,27 @@ function Contact() {
         <form
           method="POST"
           action="https://getform.io/f/b955fc50-0382-4ab9-bea4-19fcec60d47f"
-          className="pt-8 px-8 flex flex-col lg:max-w-[600px]"
+          className="pt-8 px-8 md:max-w-[600px]"
         >
           <input
             type="TEXT"
             placeholder="NAME"
             name="NAME"
-            className="bg-[#19147A] appearance-none border-2 border-white w-full py-6 px-4 text-white leading-tight focus:outline-none focus:text-white"
+            className="placeholder:text-white bg-[#19147A] appearance-none border-2 border-white w-full py-6 px-4 text-white leading-tight focus:outline-none focus:text-white"
             required
           />
           <input
             type="EMAIL"
             placeholder="EMAIL"
             name="EMAIL"
-            className="bg-[#19147A] my-4 appearance-none border-2 border-white w-full py-6 px-4 text-white leading-tight focus:outline-none focus:text-white"
+            className="placeholder:text-white bg-[#19147A] my-4 appearance-none border-2 border-white w-full py-6 px-4 text-white leading-tight focus:outline-none focus:text-white"
             required
           />
           <textarea
             name="MESSAGE"
             rows="10"
             placeholder="MESSAGE"
-            className="bg-[#19147A] appearance-none border-2 border-white w-full pt-4 py-16 px-4 text-white leading-tight focus:outline-none focus:text-white"
+            className="placeholder:text-white bg-[#19147A] appearance-none border-2 border-white w-full pt-4 py-16 px-4 text-white leading-tight focus:outline-none focus:text-white"
             required
           />
           <button className="text-left text-[#19147A] border-2 bg-white hover:bg-[#19147A] hover:border-white hover:text-white transition ease-linear duration-250 mt-4 p-4">
