3106a272afb092dc005c4bed073b466616f479eb
Musaad Hydary <musaadhydary@Mac.ht.home>
2026-05-10 02:45:52 -0400

    new portfolio

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -49,7 +49,7 @@ export default function About() {
         >
           i'm a computer engineer based in Toronto with experience in embedded
           systems and full-stack development. i like building things at every
-          level of the stack, from firmware up to the user interface! in my free
+          level of the stack, from firmware up to the user interface. in my free
           time i make lofi beats with Ableton, read slice-of-life novels, and
           play Balatro.
         </h2>
