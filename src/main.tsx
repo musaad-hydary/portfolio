3ad19af24beaf530ebd29916534b9c4e22aaea8d
Musaad Hydary <musaadhydary@Musaads-Laptop.local>
2026-05-13 13:52:46 -0400

    added additional vercel support

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { Analytics } from '@vercel/analytics/react'
 import './index.css'
-import App from './App.tsx'
+import App from './App'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
-  </StrictMode>,
+    <Analytics />
+  </StrictMode>
 )
