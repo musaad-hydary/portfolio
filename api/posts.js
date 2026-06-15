02ea4ba74fabab75c1868db46ce16e9079e3c93e
Musaad Hydary <musaadhydary@Mac.ht.home>
2026-06-14 20:04:26 -0400

    adjusted favicon

--- api/posts.js
+++ api/posts.js
@@ -4,8 +4,9 @@ export default async function handler(req, res) {
       "https://musaadh.substack.com/api/v1/posts?limit=50",
     );
     const data = await response.json();
+
     res.setHeader("Access-Control-Allow-Origin", "*");
-    res.setHeader("Cache-Control", "s-maxage=300");
+    res.setHeader("Cache-Control", "s-maxage=3600, stale-while-revalidate");
     res.status(200).json(data);
   } catch (error) {
     res.status(500).json({ error: "Failed to fetch posts" });
