ac20c6a404200788ca07c53f996d495c0c1e3861
Musaad Hydary <musaadhydary@Musaads-Laptop.local>
2026-08-11 14:23:29 -0400

    adjusted loading

--- src/components/AnimatedDots.tsx
+++ src/components/AnimatedDots.tsx
@@ -1,9 +1,9 @@
 import { useEffect, useState } from "react";
 
 export default function AnimatedDots() {
-  const [count, setCount] = useState(1);
+  const [count, setCount] = useState(0);
   useEffect(() => {
-    const id = setInterval(() => setCount((c) => (c % 3) + 1), 500);
+    const id = setInterval(() => setCount((c) => (c + 1) % 4), 500);
     return () => clearInterval(id);
   }, []);
   return <span>{".".repeat(count)}</span>;
